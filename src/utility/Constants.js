4828dcaa147707ebd8a5a72ad5285944f63b9999
hopeless1337 <38484205+hopeless1337@users.noreply.github.com>
2018-10-22 18:35:09 -0400

    Update Constants.js

--- src/utility/Constants.js
+++ src/utility/Constants.js
@@ -97,10 +97,10 @@ class Constants {
             'WEBHOOKS_UPDATE'
           ]
         },
-        game: '$help',
-        prefix: '$',
-        botOwners: ['Luner#0059', 'Ash#8999'],
-        ownerIds: ['226736342745219072', '271424970796564490']
+        game: '\$help',
+        prefix: '\$',
+        botOwners: ['hopeless#1227'],
+        ownerIds: ['167001304672108544']
       },
 
       regexes: {
